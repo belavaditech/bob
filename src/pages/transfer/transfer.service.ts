562ec53ae49ac7893984ba81bcc3c423052a7c8e
Ramesh <raga2560@gmail.com>
2018-05-04 23:08:31 +0530

    proj

--- src/pages/transfer/transfer.service.ts
+++ src/pages/transfer/transfer.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@angular/core";
-import { Http } from '@angular/http';
-
+import {Http, Headers} from "@angular/http";
 import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+
+
 
 declare var foo;
+declare var roo;
 
 
 @Injectable()
@@ -12,6 +16,7 @@ export class TransferService {
   outputtospend : any;
   outputstospend : any;
   txs : any;
+  httpitem: Observer <any>;
 
   constructor(public http: Http) {
   }
@@ -23,7 +28,6 @@ export class TransferService {
 	     address = addr.data;
       }
      var url = 'https://api.blockcypher.com/v1/btc/test3/addrs/';
-
    
     return this.http.get(url+address+"/full").map(res => res.json());
                
@@ -43,6 +47,8 @@ export class TransferService {
  
     return this.http.get(url+address+"/full").map(data =>  {
         this.txs = data.json().txs;
+        var spendabletx ;
+ 
 	for(var i=0; i< this.txs.length; i++)
         {
 	  for(var j=0; j< this.txs[i].outputs.length; j++) 
@@ -54,15 +60,23 @@ export class TransferService {
                 // This needs change when multiple transaction outputs, need to be spent
 
 	        if(this.txs[i].outputs[j].addresses[0] == address)
-  	 this.buildsingletransaction(this.txs[i].outputs[j], 
+  	      spendabletx  = this.buildsingletransaction(this.txs[i].outputs[j], 
 	 this.txs[i].hash, j, this.txs[i].inputs[0].sequence, 
 	 target, spend, amount);
                   
              }
            }
-        } return 0; } 
-        );
+        }  
+		return spendabletx  } 
+        ).flatMap(tx=> {
+             return this.sendtx(tx);
+	 // console.log(msg);
+        });
+    /*
 
+             var msg = this.sendtx(spendabletx);
+		console.log(msg);
+		 return msg; }  */
                
   }
 
@@ -85,17 +99,53 @@ var fees = 1000;
 var amount = available - commision - fees;
 var redeemScript = new foo.Buffer.Buffer(spend.redeemscript, 'hex');
 var sa ='hhh';
-alert('hi1');
+
 var tx = foo.sinkaddress.sinkaddresslib.getSignedTxToGetFundfromSA(sa,redeemScript, outscriptPubKey,
   indextospend, amount, commision, commaddr, sequence,
   tx1, spend.keyPair, foo.bitcoin.networks.testnet);
-alert('hi');
 console.log("this is test: " +tx.toHex());
-
+  return tx;
 
   }
    
+  sendtx(tx):any {
+   var pushtx = {
+    hexTx: tx
+   };
+
+
+ /*  let headers = new Headers();
+    headers.append('Origin', 'http://10.1.2.0:8100');
+    headers.append('Referer', 'http://10.1.2.0:8100/');
+
+*/
+   var url = 'https://api.blockcypher.com/v1/btc/test3/txs/push';
    
+   return this.http.post(url, JSON.stringify(pushtx) ).map(res => res.json());
+
+/*
+roo.request({method:'POST', url:url, json:JSON.stringify(pushtx)}, this.on_response)
+
+  return this.createObservable();
+*/
+
+  } 
+
+on_response(er, response, body) {
+  if(er)
+    throw er
+    console.log(JSON.stringify(body));
+  this.httpitem.next(response);
+}
+
+ createObservable() : Observable<any> {
+      return Observable.create((observer: Observer<any>) => {
+
+
+        this.httpitem = observer;
+      });
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
